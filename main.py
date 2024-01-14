865de5f95f2625e7977c9b2bc1f5f1c1947a4d45
JoelTMashana <67970779+JoelTMashana@users.noreply.github.com>
2024-01-14 19:53:27 +0000

    setup ci

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from routes.experiences import router as experiences_router
+from routes.home import router as home_router
 import os
 
 app = FastAPI()
 app.include_router(experiences_router)
+app.include_router(home_router)
 
