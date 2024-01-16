10b41cdd5226d5ae291e97cc1c2b438199c06f4f
JoelTMashana <67970779+JoelTMashana@users.noreply.github.com>
2024-01-16 05:14:46 +0000

    handle alchemy exceptions

--- services.py
+++ services.py
@@ -7,6 +7,7 @@ from models import Skill, Tool, ExperienceSkillLink, ExperienceToolLink, User
 from schemas import SkillRead, ToolRead
 from sqlalchemy.orm import Session
 from typing import List
+from sqlalchemy.exc import SQLAlchemyError
 
 
 
@@ -66,19 +67,27 @@ def query_gpt(formatted_experiences, user_query):
 
 
 
+
 def get_skills_related_to_experience(experience_id: int, db: Session) -> List[SkillRead]:
-    skills = db.query(Skill).join(ExperienceSkillLink).filter(ExperienceSkillLink.experience_id == experience_id).all()
-    skill_models = []
-    for skill in skills:
-        skill_model = SkillRead(skill_id=skill.skill_id, skill_name=skill.skill_name)
-        skill_models.append(skill_model)
-    return skill_models
+    try:
+        skills = db.query(Skill).join(ExperienceSkillLink).filter(ExperienceSkillLink.experience_id == experience_id).all()
+        skill_models = []
+        for skill in skills:
+            skill_model = SkillRead(skill_id=skill.skill_id, skill_name=skill.skill_name)
+            skill_models.append(skill_model)
+        return skill_models
+    except SQLAlchemyError as e:
+        raise HTTPException(status_code=500, detail="Database error while retrieving skills: {e}")
 
 
 def get_tools_related_to_experience(experience_id: int, db: Session) -> List[ToolRead]:
-    tools = db.query(Tool).join(ExperienceToolLink).filter(ExperienceToolLink.experience_id == experience_id).all()
-    tool_models = []
-    for tool in tools:
-        tool_model = ToolRead(tool_id=tool.tool_id, tool_name=tool.tool_name)
-        tool_models.append(tool_model)
-    return tool_models
+    try:
+        tools = db.query(Tool).join(ExperienceToolLink).filter(ExperienceToolLink.experience_id == experience_id).all()
+        tool_models = []
+        for tool in tools:
+            tool_model = ToolRead(tool_id=tool.tool_id, tool_name=tool.tool_name)
+            tool_models.append(tool_model)
+        return tool_models
+    except SQLAlchemyError as e:
+        raise HTTPException(status_code=500, detail="Database error while retrieving tools: {e}")
+
