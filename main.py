f02dad696f66f627354c42d715a56ad175a97aac
JoelTMashana <67970779+JoelTMashana@users.noreply.github.com>
2024-01-16 02:23:25 +0000

    get user experiences with associated skills and tools

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from routes.experiences import router as experiences_router
 from routes.home import router as home_router
+from routes.users import router as user_router
 import os
 
 app = FastAPI()
 app.include_router(experiences_router)
 app.include_router(home_router)
+app.include_router(user_router)
 
