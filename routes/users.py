cc74d164c7142d116d2715d98d4ff704125d74b8
JoelTMashana <67970779+JoelTMashana@users.noreply.github.com>
2024-01-17 02:00:56 +0000

    create register and login routes, auth

--- routes/users.py
+++ routes/users.py
@@ -1,11 +1,14 @@
 from typing import List
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from database import get_db
 from models import User
-from schemas import UserRead, UserCreate, UserBase
+from schemas import UserRead, UserCreate, UserBase, UserLogin
 from passlib.context import CryptContext
 import bcrypt
+from datetime import datetime, timedelta
+from jose import jwt
+import os
 
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -18,14 +21,14 @@ async def read_users(db: Session = Depends(get_db)):
     return users
 
 
+
+
 @router.post("/users/register_user", response_model=UserBase)
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
-    # Check if the user already exists
     db_user = db.query(User).filter(User.email == user.email).first()
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
 
-    # Hash the password
     hashed_password = pwd_context.hash(user.password)
 
     db_user = User(
@@ -39,3 +42,34 @@ def create_user(user: UserCreate, db: Session = Depends(get_db)):
     db.refresh(db_user)
 
     return db_user
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+@router.post("/login")
+def login(user_credentials: UserLogin, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.email == user_credentials.email).first()
+
+    if not user or not verify_password(user_credentials.password, user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email address or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token = create_access_token(data={"sub": user.email})
+
+    return {"access_token": access_token, "token_type": "bearer"}
