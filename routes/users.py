7c146c8ed20072b5eb3a069b6b48f639c0464171
JoelTMashana <67970779+JoelTMashana@users.noreply.github.com>
2024-01-17 01:12:03 +0000

    downgrade to bcrypt 3.1.7

--- routes/users.py
+++ routes/users.py
@@ -1,9 +1,14 @@
 from typing import List
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database import get_db
 from models import User
-from schemas import UserRead 
+from schemas import UserRead, UserCreate, UserBase
+from passlib.context import CryptContext
+import bcrypt
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 router = APIRouter()
 
@@ -11,3 +16,26 @@ router = APIRouter()
 async def read_users(db: Session = Depends(get_db)):
     users = db.query(User).all()
     return users
+
+
+@router.post("/users/register_user", response_model=UserBase)
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    # Check if the user already exists
+    db_user = db.query(User).filter(User.email == user.email).first()
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+
+    # Hash the password
+    hashed_password = pwd_context.hash(user.password)
+
+    db_user = User(
+        firstname=user.firstname,
+        lastname=user.lastname,
+        email=user.email,
+        hashed_password=hashed_password
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+
+    return db_user
