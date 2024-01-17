25b5fb7e85c2a7e103b6fbafb3519b1c6df46043
JoelTMashana <67970779+JoelTMashana@users.noreply.github.com>
2024-01-17 18:38:43 +0000

    test that wrong details raises 401

--- tests/test_security.py
+++ tests/test_security.py
@@ -3,12 +3,15 @@ from fastapi.testclient import TestClient
 from main import app
 from schemas import UserLogin
 from routes.users import login
+from fastapi import HTTPException
+import pytest
 
 client = TestClient(app)
 
 
 
 def test_login_successful(test_user, test_db_session):
+    
     user_credentials = UserLogin(
         email=test_user.email,
         password="plainpassword"  
@@ -18,3 +21,16 @@ def test_login_successful(test_user, test_db_session):
 
     assert "access_token" in result
     assert result["token_type"] == "bearer"
+
+
+
+def test_login_not_successful(test_user, test_db_session):
+    wrong_user_credentials = UserLogin(
+        email="wrongemail@example.com", 
+        password="wrongpassword" 
+    )
+    with pytest.raises(HTTPException) as info:
+        login(wrong_user_credentials, test_db_session)
+
+    assert info.value.status_code == 401
+
