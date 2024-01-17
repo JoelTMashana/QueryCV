8f919b5e27b546bffd2d9813112c3373c4797c3d
JoelTMashana <67970779+JoelTMashana@users.noreply.github.com>
2024-01-17 02:21:34 +0000

    move code to security module

--- routes/users.py
+++ routes/users.py
@@ -5,22 +5,17 @@ from database import get_db
 from models import User
 from schemas import UserRead, UserCreate, UserBase, UserLogin
 from passlib.context import CryptContext
-import bcrypt
-from datetime import datetime, timedelta
-from jose import jwt
-import os
+from security import verify_password, create_access_token
 
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 router = APIRouter()
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 @router.get("/users", response_model=List[UserRead])
 async def read_users(db: Session = Depends(get_db)):
     users = db.query(User).all()
     return users
-
-
 
 
 @router.post("/users/register_user", response_model=UserBase)
@@ -44,21 +39,6 @@ def create_user(user: UserCreate, db: Session = Depends(get_db)):
     return db_user
 
 
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-SECRET_KEY = os.environ.get("SECRET_KEY")
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-def create_access_token(data: dict):
-    to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-
 @router.post("/login")
 def login(user_credentials: UserLogin, db: Session = Depends(get_db)):
     user = db.query(User).filter(User.email == user_credentials.email).first()
@@ -71,5 +51,4 @@ def login(user_credentials: UserLogin, db: Session = Depends(get_db)):
         )
 
     access_token = create_access_token(data={"sub": user.email})
-
     return {"access_token": access_token, "token_type": "bearer"}
---
+++ security.py
@@ -0,0 +1,21 @@
+from passlib.context import CryptContext
+from jose import jwt
+import os
+from datetime import datetime, timedelta
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
