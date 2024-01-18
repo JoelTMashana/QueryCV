6063270418af104aad6b9d0c001a575182d2beaa
JoelTMashana <67970779+JoelTMashana@users.noreply.github.com>
2024-01-18 15:51:01 +0000

    test tool user link

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,8 +1,8 @@
 from fastapi.testclient import TestClient
 from main import app  
-from models import User, Skill, UserSkillLink
-from schemas import UserSkills
-from routes.users import link_skills_to_user
+from models import User, Skill, UserSkillLink, Tool, UserToolLink
+from schemas import UserSkills, UserTools
+from routes.users import link_skills_to_user, link_tools_to_user
 
 
 client = TestClient(app)
@@ -15,20 +15,37 @@ def test_read_home():
 
 
 def test_link_skills_to_user(test_user, test_db_session):
-    # test_user = User(firstname="Test", lastname="User", email="test@example.com", hashed_password="hashed_test")
     test_db_session.add(test_user)
     test_db_session.commit()
 
-    test_skill_one = Skill(skill_name="Python")
-    test_skill_two = Skill(skill_name="JavaScript")
+    test_skill_one = Skill(skill_name="Communication")
+    test_skill_two = Skill(skill_name="Teamwork")
     test_db_session.add(test_skill_one)
     test_db_session.add(test_skill_two)
     test_db_session.commit()
 
     skills = UserSkills(skill_ids=[test_skill_one.skill_id, test_skill_two.skill_id])
 
-    response = link_skills_to_user(user_id=test_user.user_id, skills_selected_by_user=skills, db=test_db_session)
+    result = link_skills_to_user(user_id=test_user.user_id, skills_selected_by_user=skills, db=test_db_session)
 
     linked_skills = test_db_session.query(UserSkillLink).filter(UserSkillLink.user_id == test_user.user_id).all()
     assert len(linked_skills) == 2
-    assert response == {"message": "Skills linked to user successfully"}
+    assert result == {"message": "Skills linked to user successfully"}
+
+
+def test_link_tools_to_user(test_user, test_db_session):
+    test_db_session.add(test_user)
+    test_db_session.commit()
+
+    test_tool_one = Tool(tool_name="Python")
+    test_tool_two = Tool(tool_name="JavaScript")
+    test_db_session.add(test_tool_one)
+    test_db_session.add(test_tool_two)
+    test_db_session.commit()
+
+    tools = UserTools(tool_ids=[test_tool_one.tool_id, test_tool_two.tool_id])
+    result = link_tools_to_user(user_id=test_user.user_id, tools_selected_by_user=tools, db=test_db_session)
+
+    linked_tools = test_db_session.query(UserToolLink).filter(UserToolLink.user_id == test_user.user_id).all()
+    assert len(linked_tools) == 2
+    assert result == {"message": "Tools linked to user successfully"}
