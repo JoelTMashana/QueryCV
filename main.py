f866746d5199feffe57a7b9c386ddb5e17794662
JoelTMashana <67970779+JoelTMashana@users.noreply.github.com>
2024-01-27 20:40:15 +0000

    allow CORS

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.experiences import router as experiences_router
 from routes.home import router as home_router
 from routes.users import router as user_router
 from routes.tools_and_skills import router as tools_and_skills_router
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"], 
+    allow_headers=["Content-Type", "Authorization"],  
+)
+
 app.include_router(experiences_router)
 app.include_router(home_router)
 app.include_router(user_router)
