f1d338365039446865ad88b990e2798cf0d001e9
JoelTMashana <67970779+JoelTMashana@users.noreply.github.com>
2024-02-08 18:18:17 +0000

    allow all

--- main.py
+++ main.py
@@ -10,17 +10,17 @@ load_dotenv()
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:3000",
-    "https://query-cv.vercel.app"
-]
+# origins = [
+#     "http://localhost:3000",
+#     "https://query-cv.vercel.app"
+# ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  
+    allow_origins=["*"],  
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "PATCH"], 
-    allow_headers=["Content-Type", "Authorization"],  
+    allow_methods=["*"], 
+    allow_headers=["*"],  
 )
 
 app.include_router(experiences_router)
